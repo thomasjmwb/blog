8f5eab76dcd0834939492f63ede0468cacae0ecc
Zarah <zaraheadie@live.co.uk>
2016-09-16 19:18:11 +0100

    Added basic http authentication to articles and comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	http_basic_authenticate_with name: "zarah", password: "a", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
