88342c524fc9898ac16adfbd4adca4f4f3ef49a3
Zarah <zaraheadie@live.co.uk>
2016-09-16 17:30:00 +0100

    All CRUD functions and views completed and actionable on articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,18 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+		def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 
@@ -14,13 +26,24 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	def show
+	def update
 		@article = Article.find(params[:id])
+
+	if @article.update(article_params)
+		redirect_to @article 
+	else
+		render 'edit'	
 	end
 
-	def new
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+	
 	private
 		def article_params
 			params.require(:article).permit(:title, :text)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-	validates :title, presence: true,
+	validates :title, uniqueness: true, presence: true,
 	length: { minimum: 5}
 end
