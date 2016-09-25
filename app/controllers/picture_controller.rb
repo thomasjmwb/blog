dfcb53605237104a7d2569adc98adb57664df169
Zarah <zaraheadie@live.co.uk>
2016-09-25 22:07:51 +0100

    Added picture display and deleting picture from article functionality

--- app/controllers/picture_controller.rb
+++
@@ -1,4 +0,0 @@
-class PictureController < ApplicationController
-  def index
-  end
-end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,6 +8,13 @@ end
     @picture = @article.pictures.create(picture_params)
     redirect_to article_path(@article)
   end
+
+def destroy
+  @article = Article.find(params[:article_id])
+  @picture = @article.pictures.find(params[:id])
+  @picture.destroy
+  redirect_to article_path(@article)
+end
  
   private
     def picture_params
